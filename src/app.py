30ccfb060a423f2deca4e9de3bfa559218110e77
Константин Михайлович Вороной <konstantinvoronoy@gmail.com>
2022-03-15 20:34:16 +0600

    cleaning up

--- src/app.py
+++ src/app.py
@@ -7,21 +7,27 @@ from src.bot import bot
 import time
 
 def create_app():
-    app = FastAPI()
-    
-    app.add_middleware(DBSessionMiddleware, db_url=config.db_url)
+    # Initialising fastapi app
+    app = FastAPI()  
+    # Add sqlalchemy as middleware
+    app.add_middleware(DBSessionMiddleware, db_url=config.db_url)  
+    # Add cors middleware
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[str(origin) for origin in config.origins],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
-    )
-        
+    ) 
+    # Include router
     app.include_router(router)
-
+    # Remove current webhook in telegram bot
     bot.remove_webhook()
+    # Wait a bit
     time.sleep(5)
+    # Set a new webhook
     bot.set_webhook(url=config.webhook_url)
+    # Showing info of this new webhook
     print(bot.get_webhook_info())
+
     return app
--- src/database.py
+++
@@ -1,5 +0,0 @@
-from src.config import Config as config
-from sqlalchemy import create_engine
-
-engine = create_engine(config.db_url)
-connection = engine.connect()
